89cc9b7c86ba0056b483d252fa7c51d6794c2f35
Dawood <md1dawood@gmail.com>
2021-06-12 14:04:36 +0530

    donate page

--- khudai_imdad/urls.py
+++ khudai_imdad/urls.py
@@ -22,6 +22,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
+    path('donate/', views.DonatePage.as_view(), name='donate'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('cases/', include("cases.urls", namespace="cases")),
