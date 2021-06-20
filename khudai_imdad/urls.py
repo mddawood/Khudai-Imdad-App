980073507e93488b25bee175f0915177b3f3f5da
Dawood <md1dawood@gmail.com>
2021-06-20 16:04:14 +0530

    'static'

--- khudai_imdad/urls.py
+++ khudai_imdad/urls.py
@@ -32,3 +32,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+from django.conf import settings ... if settings.DEBUG: urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
